f24ff151574dea6484d77209d1c417deda92b296
jSunnari <jsunnari@gmail.com>
2016-04-12 16:53:16 +0200

    First try.

---
+++ src/main/java/com/sunnari/pi4watering/App.java
@@ -0,0 +1,19 @@
+package com.sunnari.pi4watering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Jonas on 2016-04-12.
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+
+
+
+    }
+}
---
+++ src/main/java/com/sunnari/pi4watering/controller/WateringController.java
@@ -0,0 +1,33 @@
+package com.sunnari.pi4watering.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Jonas on 2016-04-12.
+ */
+
+public class WateringController {
+
+    private static GpioController gpioController = GpioFactory.getInstance();
+    private static GpioPinDigitalOutput pump1 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_04, "pump1", PinState.LOW);
+    private static GpioPinDigitalOutput pump2 = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_05, "pump2", PinState.LOW);
+
+
+    //When the application runs on the raspberry pi, send a message (to be sure everything is OK):
+    @RequestMapping("/")
+    public String greeting(){
+        return "Hello world!";
+    }
+
+    @RequestMapping("/pump")
+    public String powerOnPump1(){
+        pump1.high();
+        return "All good!";
+    }
+    @RequestMapping("/pump")
+    public String powerOffPump1(){
+        pump1.low();
+        return "All good!";
+    }
+}

5139a2d2dc35eb0d755d62ef16d673d0c0995495
jSunnari <jsunnari@gmail.com>
2016-04-18 19:50:54 +0200

    Configuration for websecurity

--- src/main/java/com/sunnari/pi4watering/config/WebSecurityConfig.java
+++ src/main/java/com/sunnari/pi4watering/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+
     }
 
     @Autowired

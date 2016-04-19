c2efc7f9663db8c49aade6d125f496cf88bbedc9
jSunnari <jsunnari@gmail.com>
2016-04-19 13:40:18 +0200

    Testing "Get latest runs"-table.

--- src/main/java/com/sunnari/pi4watering/App.java
+++ src/main/java/com/sunnari/pi4watering/App.java
@@ -1,9 +1,11 @@
 package com.sunnari.pi4watering;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.sql.SQLException;
 import java.util.TimeZone;
 
 /**
@@ -17,6 +19,14 @@ public class App {
     public static void main(String[] args) {
         TimeZone tzone = TimeZone.getTimeZone("Europe/Stockholm");
         TimeZone.setDefault(tzone);
+
+        try {
+            Server webServer = Server.createWebServer("-web,-webAllowOthers,-webPort,8082").start();
+            Server server = Server.createTcpServer("-tcp,-tcpAllowOthers,-tcpPort,9092").start();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         SpringApplication.run(App.class, args);
 

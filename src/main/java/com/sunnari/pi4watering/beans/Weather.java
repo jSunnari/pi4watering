3e70494533d6e2c53d9befe9159a96f32967c837
jSunnari <jsunnari@gmail.com>
2016-04-19 12:59:42 +0200

    Testing "Get latest runs"-table.

--- src/main/java/com/sunnari/pi4watering/beans/Weather.java
+++ src/main/java/com/sunnari/pi4watering/beans/Weather.java
@@ -26,7 +26,7 @@ public class Weather {
     }
 
     public String temperature(){
-        return daily.getDay(0).cloudCover().toString();
+        return daily.getDay(0).temperature().toString();
     }
 
     public String summary(){

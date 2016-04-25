51559ad7d33f8da2fa01c16967d5ecdbcaacdf1d
jSunnari <jsunnari@gmail.com>
2016-04-25 19:10:59 +0200

    Adjusting weather-icon

--- src/main/java/com/sunnari/pi4watering/controller/DatabaseController.java
+++ src/main/java/com/sunnari/pi4watering/controller/DatabaseController.java
@@ -4,8 +4,10 @@ import com.sunnari.pi4watering.domain.PumpRunRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Jonas on 2016-04-19.
@@ -22,5 +24,11 @@ public class DatabaseController {
     public String listPumpRuns(Model model) {
         model.addAttribute("pumpruns", repository.findAll());
         return "list";
+    }
+
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable long id) {
+        repository.delete(id);
+        return new ModelAndView("redirect:/lists");
     }
 }
